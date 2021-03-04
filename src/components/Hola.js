ccaa58b84b597735dbaf036277bcbf86e6a425f8
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2021-03-04 13:20:06 -0300

    addition: added Hola component to practice

---
+++ src/components/Hola.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hola = () => {
+  const nombre = "Pepe"
+
+  return (
+    <>
+      <h2>Hola desde componente {nombre}</h2>
+      <p>Texto desde Hola</p>
+    </>
+  );
+};
+
+export default Hola;
