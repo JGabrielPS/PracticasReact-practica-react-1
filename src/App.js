73d178b3191735d2a5d40fee5cd04ded6f9529af
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-05 15:54:29 -0300

    addition: added Listado, Contador and Temperatura component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import Hola from './components/Hola'
+import React from 'react';
+import "./App.css";
+
+// import Hola from "./components/Hola";
+// import Contador from "./components/Contador";
+// import Listado from "./components/Listado";
+import Temperatura from "./components/Temperatura";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App mt-5">
       <h1>Titulo de nuestro primer proyecto</h1>
-      <Hola />
+      {/* <Hola /> */}
+      {/* <Contador /> */}
+      {/* <Listado /> */}
+      <Temperatura />
     </div>
   );
 }
